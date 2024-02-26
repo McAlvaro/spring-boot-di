490bd9ac516b8cee436d72a5ac273c13837d749b
McAlvaro <mc.alv641@gmail.com>
2024-02-26 00:39:34 -0400

    Added component life cycle

--- src/main/java/com/mcalvaro/springbootdi/domain/Invoice.java
+++ src/main/java/com/mcalvaro/springbootdi/domain/Invoice.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class Invoice {
  
@@ -17,6 +19,12 @@ public class Invoice {
 
     @Autowired
     private List<ItemInvoice> items;
+
+    @PostConstruct
+    public void initComponent() {
+
+        System.out.println("Component Construido");
+    }
 
 	public String getDescription() {
 		return description;
